37986f528971fcb9ac8398ed957eaa8e003f7bb1
Nick Zigler <nzigler@gmail.com>
2018-10-08 19:58:29 -0700

    Build 2

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import * as mapboxgl from "mapbox-gl";
 import settings from "./settings.json";
 
-let map;
+let map; //do not use "var". only const if the value is never going to change again
 
 async function init() {
     const custom = await import("./custom-style.json");
     const style = map.getStyle();
+    const catchments = await import("../data/scc_catchments.json");
+    const lid = await import("../data/scc_lid_pts_wgs3.json");
 
     style.sources = {
         ...style.sources,
@@ -15,6 +17,11 @@ async function init() {
     };
     style.layers.push(...custom.layers);
     map.setStyle(style);
+
+    map.getSource("catchments").setData(catchments);
+    map.getSource("catchments").setData(catchments);
+    map.getSource("lid").setData(lid);
+
 }
 
 mapboxgl.accessToken = settings.accessToken;
--- src/process.mjs
+++ src/process.mjs
@@ -1,1 +1,24 @@
-console.log("success. 👍");
+import fs from "fs";
+import turf from "@turf/turf";
+import neighborhoods from "../data/scc_catchments.json";
+import lid from "../data/scc_lid_pts_wgs3.json";
+
+
+let output = turf.collect(neighborhoods, lid, 'Final_Area', 'TotalArea');
+
+output.features = output.features.filter(function(feature, i) {
+   feature.id = i;
+   feature.properties.TotalArea = feature.properties.TotalArea.reduce(function(value, total) {
+     return value + total;
+   }, 0);
+   return feature.properties.TotalArea > 0;
+});
+
+
+
+output = JSON.stringify(output, null, "\t");
+fs.writeFile("../data/outupt.json", output, function(err) {
+	if (err) throw err;
+
+  console.log("success. 👍");
+});
